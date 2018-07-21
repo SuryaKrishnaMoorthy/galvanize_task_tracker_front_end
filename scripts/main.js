f4b6d8d1e30107ef9a2ab43e423871fdeea00941
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-07-21 00:24:52 -0700

    require and use some validation functions in signup and login buttons

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+const validation = require('./validate.js');
+
+const loginBtn = document.querySelector('.login-btn')
+const signupBtn = document.querySelector('.signup-btn')
+
+loginBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const email = document.getElementById('login-email').value
+  const password = document.getElementById('login-password').value
+
+  if(!email || !password) validation.shakeNode(event.target);
+  loginUser(email, password)
+})
+
+signupBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const first_name = document.getElementById('signup-fName').value
+  const last_name = document.getElementById('signup-lName').value
+  const email = document.getElementById('signup-email').value
+  const password = document.getElementById('signup-password').value
+
+  if(!first_name || !last_name || !email || !password) validation.shakeNode(event.target);
+  validateUser(first_name, last_name, email, password)
+})
+
+function loginUser (email, password) {
+  axios.post('https://auth-task-manager-server.herokuapp.com/api/users/login', { email, password })
+  .then(response => {
+    const token = response.data.token
+    document.location.replace("./views/tasks.html")
+  })
+  .catch(e => {
+    validation.showAndFadeError(e);
+    throw new Error(e)
+  })
+}
+
+function validateUser (first_name, last_name, email, password) {
+  axios.post('https://auth-task-manager-server.herokuapp.com/api/users/signup', { first_name, last_name, email, password })
+  .then(response => {
+    if (response) loginUser(email, password)
+  })
+  .catch(e => {
+    validation.showAndFadeError(e);
+    throw new Error(e) 
+  })
+}
